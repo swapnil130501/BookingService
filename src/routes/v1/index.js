bff4758c095e6cd00e28fc802820d9640c6cae2a
swapnil <swapnil130501@gmail.com>
2023-06-07 16:22:22 +0530

    api gateway setup

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -9,6 +9,11 @@ const { BookingController } = require('../../controllers/index');
 
 const bookingController = new BookingController();
 
+router.get('/info', (req, res) => {
+    return res.json({
+        message: 'Response from routes'
+    });
+});
 router.post('/bookings', bookingController.create);
 router.post('/publish', bookingController.sendMessageToQueue);
 
